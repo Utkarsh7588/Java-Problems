0a8faf61cfaa09f3c620ed6e1e6849cb4f03630b
UVM <utkarshmore7588@gmail.com>
2023-09-16 17:45:54 +0530

    all possible subsets using recurssion

--- Recurssion problems/Strings/Subsets.java
+++ Recurssion problems/Strings/Subsets.java
@@ -1,2 +1,17 @@
-package Strings;public class Subsets {
+package Strings;
+
+public class Subsets {
+    public static void main(String[] args) {
+        findSS("abc","");
+    }
+    public static void findSS(String str, String sk){
+        if(str.isEmpty()){
+            System.out.println(sk);
+            return;
+        }
+
+        findSS(str.substring(1),sk+str.charAt(0));
+        findSS(str.substring(1),sk);
+
+    }
 }
