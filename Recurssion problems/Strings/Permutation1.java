ae5876fe9d8bd009ec8a61c58280ec3a7da2a3f7
UVM <utkarshmore7588@gmail.com>
2023-09-17 19:09:16 +0530

    permutation by another method

--- Recurssion problems/Strings/Permutation1.java
+++ Recurssion problems/Strings/Permutation1.java
@@ -1,2 +1,48 @@
-package Strings;public class Permutation1 {
+package Strings;
+
+import java.util.ArrayList;
+
+public class Permutation1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList=permutatearr("abc",0,2);
+        System.out.println(arrayList);
+    }
+    public static void permutate(String str, int l, int r){
+        if (l==r){
+            System.out.println(str);
+        }
+        else {
+            for (int i = l; i <=r; i++) {
+                str = swap(str, l, i);
+                permutate(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    public static ArrayList<String> permutatearr(String str, int l, int r){
+        ArrayList<String> list=new ArrayList<String>();
+        if (l==r){
+            list.add(str);
+            return list;
+        }
+        else {
+            ArrayList<String> ans=new ArrayList<>();
+            for (int i = l; i <=r; i++) {
+
+                str = swap(str, l, i);
+                ans.addAll(permutatearr(str, l + 1, r));
+                str = swap(str, l, i);
+            }
+            return ans;
+        }
+    }
+    public static String swap(String str, int i, int j){
+        char temp;
+        char[] s=str.toCharArray();
+        temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        return String.valueOf(s);
+    }
 }
